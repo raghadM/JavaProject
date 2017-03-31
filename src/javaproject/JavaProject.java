ce37bd110e3b27d361ff48c819244739c2c72a89
Sara Saleh <sara.sale7@me.com>
2017-03-31 18:16:29 +0300

    Adding classes

--- src/javaproject/JavaProject.java
+++ src/javaproject/JavaProject.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaproject;
 
-/**
- *
- * @author SARA
- */
+import javax.swing.JFrame;
+
+class SignFrame extends JFrame {
+    
+}
+
+class ReservationFrame extends JFrame {
+    
+}
+
+
 public class JavaProject {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+
     }
     
 }
